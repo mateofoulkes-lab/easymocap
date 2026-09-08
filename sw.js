a150b93baa0a318076f8d3e57997e710cc8c3175
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-09-08 15:10:28 -0300

    Bump PWA cache to v0.1.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="easymocap-v015";
-const LOCAL=["./","./index.html","./web/styles.css","./web/app.js?v=0.1.5","./manifest.webmanifest"];
+const CACHE="easymocap-v016";
+const LOCAL=["./","./index.html","./web/styles.css","./web/app.js?v=0.1.6","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
