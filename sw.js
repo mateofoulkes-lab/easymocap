aa7d0c7e84bb7b4e456351cebb6534af478e0d31
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-09-07 22:21:15 -0300

    Add sw.js for mobile web

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="easymocap-v1";
+const LOCAL=["./","./index.html","./web/styles.css","./web/app.js","./manifest.webmanifest"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
